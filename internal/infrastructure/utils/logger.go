13aad822f70e593798d989cbfa0d7e8a958a5075
yuusufyan <yuusufyan13@gmail.com>
2026-01-08 11:36:20 +0700

    FEAT : Update Logger utils

--- internal/infrastructure/utils/logger.go
+++ internal/infrastructure/utils/logger.go
@@ -2,16 +2,39 @@ package utils
 
 import (
 	"log"
+	"os"
 	"time"
 )
 
-func ErrorLog(message string, err error, panic bool) {
-	log.Printf("ERROR: %s - %v", message, err)
-	if panic {
-		log.Fatalf("[IPROC-BE] [ERROR] [%v] %v because %v", time.Now().Format("2006-01-02 15:04:05"), message, err)
+type Logger struct {
+	base *log.Logger
+}
+
+func NewLogger() *Logger {
+	return &Logger{
+		base: log.New(os.Stdout, "", 0),
 	}
 }
 
-func InfoLog(message string) {
-	log.Printf("[IPROC-BE] [INFO] [%v] %v", time.Now().Format("2006-01-02 15:04:05"), message)
+func (l *Logger) Error(message string, err error, fatal bool) {
+	l.base.Printf("[ERROR] [%s] %s : %v",
+		time.Now().Format("2006-01-02 15:04:05"),
+		message,
+		err,
+	)
+
+	if fatal {
+		l.base.Fatalf("[FATAL] [%s] %s : %v",
+			time.Now().Format("2006-01-02 15:04:05"),
+			message,
+			err,
+		)
+	}
+}
+
+func (l *Logger) Info(message string) {
+	l.base.Printf("[INFO] [%s] %s",
+		time.Now().Format("2006-01-02 15:04:05"),
+		message,
+	)
 }
