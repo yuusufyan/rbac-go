72c2e6bad427a2f265b4f6c21eb595dbcc3d3f8b
yuusufyan <yuusufyan13@gmail.com>
2026-01-09 10:21:23 +0700

    FEAT : Add load .env using config file

--- cmd/main.go
+++ cmd/main.go
@@ -4,13 +4,20 @@ import (
 	"github.com/gofiber/fiber/v2"
 	fiberLogger "github.com/gofiber/fiber/v2/middleware/logger"
 
+	"rbac-go/internal/infrastructure/configs"
 	"rbac-go/internal/infrastructure/utils"
 )
 
 func main() {
 	log := utils.NewLogger()
 
+	configs.LoadEnv()
+	configs.LoadConfig()
 	app := fiber.New()
+
+	if configs.AppConfig == nil {
+		panic("CONFIG IS NIL — LoadConfig() FAILED")
+	}
 
 	app.Use(fiberLogger.New(fiberLogger.Config{
 		Format:     "[${time}] ${ip} - ${status} ${method} ${path}\n",
@@ -20,5 +27,5 @@ func main() {
 
 	log.Info("Server starting...")
 
-	app.Listen(":3000")
+	app.Listen(":" + configs.AppConfig.AppPort)
 }
